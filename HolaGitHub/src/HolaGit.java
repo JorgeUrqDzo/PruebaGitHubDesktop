f273436d6d34a110d0b46fe569ca3070c17fa24c
jorgeurqdzo <jorgedzo_17@hotmail.com>
2015-11-05 21:18:19 -0700

    Creacion de carpeta src con clase HolaGit

---
+++ HolaGitHub/src/HolaGit.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jorge on 05/11/15.
+ */
+public class HolaGit {
+    public static void main(String[] args) {
+        System.out.println("Hola GitHub desde intellij IDEA");
+    }
+}
